dcf71926896c9ed47a7afd1f71ebe3249daf63e3
Hrushikesh Reddy <hrushikesh200802@gmail.com>
2024-11-21 10:30:31 +0530

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,6 @@ import { blogRouter } from "./routes/blog";
 const app = new Hono();
 
 app.route("/api/v1/user", userRouter);
-app.route("/api/v1/post", blogRouter);
+app.route("/api/v1/user", blogRouter);
 
 export default app;
